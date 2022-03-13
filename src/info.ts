00ca273b62f2b2bff39bcc025d7b26746d633c95
Evan Elias Young <evanyoung762@gmail.com>
2022-03-13 18:24:10 -0500

    :bookmark: 0.2.1

--- src/info.ts
+++ src/info.ts
@@ -4,7 +4,7 @@
  *
  * @author    Evan Elias Young
  * @date      2022-03-09
- * @date      2022-03-11
+ * @date      2022-03-13
  * @copyright Copyright 2022 Evan Elias Young. All rights reserved.
  */
 
@@ -14,7 +14,7 @@ import { DateTime } from 'luxon';
 export namespace BotInfo {
   export const tagline = `Triangle-Chan: The Tenaciously Tasteful Discord Tease`;
   export const library = '[discord.ts / Node](https://github.com/oceanroleplay/discord.ts)';
-  export const version = '0.2.0';
-  export const date = DateTime.fromObject({ year: 2022, month: 3, day: 11 });
+  export const version = '0.2.1';
+  export const date = DateTime.fromObject({ year: 2022, month: 3, day: 13 });
   export const activity: ActivitiesOptions = { name: 'Triangle', type: 'WATCHING' };
 }
