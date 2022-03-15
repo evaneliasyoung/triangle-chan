3b0cd419e55d768daf18a875822a5247129f5ee1
Evan Elias Young <evanyoung762@gmail.com>
2022-03-14 21:03:20 -0500

    :bookmark: 0.3.2

--- src/info.ts
+++ src/info.ts
@@ -12,7 +12,7 @@ export namespace BotInfo {
   export const tagline = `Triangle-Chan: The Tenaciously Tasteful Discord Tease`;
   export const library =
     '[discord.ts / Node](https://github.com/oceanroleplay/discord.ts)';
-  export const version = '0.3.2-b2';
+  export const version = '0.3.2';
   export const date = DateTime.fromObject({year: 2022, month: 3, day: 14});
   export const activity: ActivitiesOptions = {
     name: 'Triangle',
