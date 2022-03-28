8e0738649456dcae6177c10967cef650f8e3449d
Evan Elias Young <evanyoung762@gmail.com>
2022-03-28 18:54:34 -0500

    🔖 0.3.3

--- src/info.ts
+++ src/info.ts
@@ -10,8 +10,8 @@ export const BotInfo = {
   source: '[GitHub](https://github.com/evaneliasyoung/triangle-chan)',
   tagline: 'Triangle-Chan: The Tenaciously Tasteful Discord Tease',
   library: '[discord.ts / Node](https://github.com/oceanroleplay/discord.ts)',
-  version: '0.3.2',
-  date: DateTime.fromObject({year: 2022, month: 3, day: 14}),
+  version: '0.3.3',
+  date: DateTime.fromObject({year: 2022, month: 3, day: 28}),
   activity: {
     name: 'Triangle',
     type: 'WATCHING',
